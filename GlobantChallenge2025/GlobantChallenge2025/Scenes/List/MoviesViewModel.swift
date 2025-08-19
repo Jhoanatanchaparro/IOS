ebde4c0bb5623ecab9f14413a0faa6546a5be236
Jhonatan Chaparro <jhonatand_chaparro@soy.sena.edu.co>
2025-08-19 14:35:53 -0500

    ...

--- GlobantChallenge2025/GlobantChallenge2025/Scenes/List/MoviesViewModel.swift
+++ GlobantChallenge2025/GlobantChallenge2025/Scenes/List/MoviesViewModel.swift
@@ -36,25 +36,19 @@ final class MoviesViewModel: ObservableObject {
     func toggleFavorite(for movie: Movie) {
         interactor.toggleFavorite(movie)
 
-        if isFavoriteView {
-            loadMovies(forceReload: true)
-        } else {
-            if let index = movies.firstIndex(where: { $0.id == movie.id }) {
-                var updatedMovie = movies[index]
-                updatedMovie.isFavorite.toggle()
-                movies[index] = updatedMovie
-            }
-        }
+        isFavoriteView ? loadMovies(forceReload: true) : updateMovieFavoriteStatus(for: movie)
+    }
+    
+    private func updateMovieFavoriteStatus(for movie: Movie) {
+        guard let index = movies.firstIndex(where: { $0.id == movie.id }) else { return }
+        var updatedMovie = movies[index]
+        updatedMovie.isFavorite.toggle()
+        movies[index] = updatedMovie
     }
     
     var filteredMovies: [Movie] {
         let query = searchText.normalized().trimmingCharacters(in: .whitespacesAndNewlines)
-
-        guard !query.isEmpty else {
-            return movies
-        }
-
-        return movies.filter { movie in
+        return query.isEmpty ? movies : movies.filter { movie in
             movie.title.normalized().contains(query) ||
             (movie.translatedTitle?.normalized().contains(query) ?? false)
         }
